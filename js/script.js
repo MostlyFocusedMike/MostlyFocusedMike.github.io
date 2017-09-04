7aa122b1ec10a0cea381c427d329a75a7db77097
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2017-09-04 15:32:01 -0400

    Third time's the charm with sitcky hover fix

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@
 (function () {
   "use strict";   
   var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
-  if (touchsupport){
-      document.documentElement.className += "has-touch"
+  if (!touchsupport){
+      document.documentElement.className += "no-touch"
   }
 
 
